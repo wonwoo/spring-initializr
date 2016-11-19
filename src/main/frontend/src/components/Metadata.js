831d2d68fab2c1c97eb5f7e234e5b84acb4a05a4
wonwoo <aoruqjfu@gmail.com>
2016-11-19 21:14:36 +0900

    label 변경

--- src/main/frontend/src/components/Metadata.js
+++ src/main/frontend/src/components/Metadata.js
@@ -18,13 +18,21 @@ export default class Metadata extends Component {
     getStateStore() {
         return {
             artifactId :"",
+            artifactTitle :"",
             groupId : "",
+            groupTitle : "",
             name : "",
+            nameTitle : "",
             description : "",
+            descriptionTitle : "",
             packageName : "",
+            packageNameTitle : "",
             packagings : [],
+            packagingsTitle : "",
             languages : [],
-            javaVersions : []
+            languagesTitle : "",
+            javaVersions : [],
+            javaVersionsTitle : "",
         }
     }
 
@@ -40,13 +48,21 @@ export default class Metadata extends Component {
     _onChange() {
         this.setState({
             artifactId : MetaDataStore.ArtifactId.content,
+            artifactTitle : MetaDataStore.ArtifactId.title,
             groupId : MetaDataStore.GroupId.content,
+            groupTitle : MetaDataStore.GroupId.title,
             name : MetaDataStore.Name.content,
+            nameTitle : MetaDataStore.Name.title,
             description : MetaDataStore.Description.content,
+            descriptionTitle : MetaDataStore.Description.title,
             packageName : MetaDataStore.PackageName.content,
+            packageNameTitle : MetaDataStore.PackageName.title,
             packagings : MetaDataStore.Packagings.content,
+            packagingsTitle : MetaDataStore.Packagings.title,
             languages : MetaDataStore.Languages.content,
-            javaVersions : MetaDataStore.javaVersions.content
+            languagesTitle : MetaDataStore.Languages.title,
+            javaVersions : MetaDataStore.javaVersions.content,
+            javaVersionsTitle : MetaDataStore.javaVersions.title
         });
     }
 
@@ -76,35 +92,35 @@ export default class Metadata extends Component {
                 <h2>Project Metadata</h2>
                 <p>Artifact coordinates</p>
                 <div className="form-group groupid-form-group">
-                    <label htmlFor="groupId" className="control-label">Group</label>
+                    <label htmlFor="groupId" className="control-label">{this.state.groupTitle}</label>
                     <input id="groupId" className="form-control" type="text" value={this.state.groupId} onChange={this.handleGroupIdChange}
                            name="groupId" />
                 </div>
                 <div className="form-group artifactid-form-group">
-                    <label htmlFor="artifactId" className="control-label">Artifact</label>
+                    <label htmlFor="artifactId" className="control-label">{this.state.artifactTitle}</label>
                     <input id="artifactId" className="form-control" type="text" onChange={this.handleArtifactIdChange}
                            value={this.state.artifactId}
                            name="artifactId" />
                 </div>
                 <div className="form-group full hidden name-form-group">
-                    <label htmlFor="name" className="control-label">Name</label>
+                    <label htmlFor="name" className="control-label">{this.state.nameTitle}</label>
                     <input id="name" className="form-control" type="text" value={this.state.name} onChange={this.handleNameChange}
                            name="name" />
                 </div>
                 <div className="form-group full hidden description-form-group">
-                    <label htmlFor="description" className="control-label">Description</label>
+                    <label htmlFor="description" className="control-label">{this.state.descriptionTitle}</label>
                     <input id="description" className="form-control" type="text" onChange={this.handleDescriptionChange}
                            value={this.state.description} name="description" />
                 </div>
                 <div className="form-group full hidden packagename-form-group">
-                    <label htmlFor="packageName" className="control-label">PackageName</label>
+                    <label htmlFor="packageName" className="control-label">{this.state.packageNameTitle}</label>
                     <input id="packageName" className="form-control" type="text" onChange={this.handlePackageNameChange}
                            value={this.state.packageName}
                            name="packageName" />
 
                 </div>
                 <div className="form-group full hidden packaging-form-group">
-                    <label htmlFor="packaging" className="control-label">Packagings</label>
+                    <label htmlFor="packaging" className="control-label">{this.state.packagingsTitle}</label>
                     <select className="form-control" id="packaging" name="packaging">
                         {
                             this.state.packagings.map((content, idx) => {
@@ -115,7 +131,7 @@ export default class Metadata extends Component {
                     </select>
                 </div>
                 <div className="form-group full hidden javaversion-form-group">
-                    <label htmlFor="javaVersion" className="control-label">javaVersions</label>
+                    <label htmlFor="javaVersion" className="control-label">{this.state.javaVersionsTitle}</label>
                     <select className="form-control" name="javaVersion" id="javaVersion">
                         {
                             this.state.javaVersions.map((content, idx) => {
@@ -126,7 +142,7 @@ export default class Metadata extends Component {
                     </select>
                 </div>
                 <div className="form-group full hidden language-form-group">
-                    <label htmlFor="language" className="control-label">Languages</label>
+                    <label htmlFor="language" className="control-label">{this.state.languagesTitle}</label>
                     <select className="form-control" name="language" id="language">
                         {
                             this.state.languages.map((content, idx) => {
